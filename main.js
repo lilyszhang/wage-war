f5cabb39c608018df9e579ae3e2ec512dcddc6e2
Lily Zhang <lilyszhang96@gmail.com>
2017-04-08 03:37:14 -0500

    woo

--- main.js
+++ main.js
@@ -1,90 +1,178 @@
 $('#payRatio').remove();
 document.onload = renderRatioChart();
 
+$('#ratioChart').on("click", function() {
+  $('#payRatio').remove()
+  $('#hoursC').remove()
+  renderRatioChart();
+  $(this).addClass('highlight');
+  $('#hoursChart').removeClass('highlight');
+})
+
+$('#hoursChart').on("click", function() {
+  $('#payRatio').remove()
+  $('#hoursC').remove()
+  renderHoursChart();
+  $(this).addClass('highlight');
+  $('#ratioChart').removeClass('highlight');
+})
+
 function renderRatioChart() {
   $('#hoursC').remove();
   $('#graph-container').append('<canvas id="payRatio"><canvas>');
   var payRatio = document.getElementById('payRatio').getContext('2d');
   payRatio.canvas.width = window.innerWidth;
-  payRatio.canvas.height = window.innerHeight-40;
+  payRatio.canvas.height = window.innerHeight-120;
   var data = [{
       type: 'bubble',
       label: 'Gender Pay Ratio',
       data: [{
         //Management Occupations
-        x: 80028, y: 59696, r: 6
+        x: 80028,
+        y: 59696,
+        r: 6,
+        label: 'Management'
       }, {
         //Business and Financial Operations Occupations
-        x: 71396, y: 52936, r: 6
+        x: 71396,
+        y: 52936,
+        r: 6,
+        label: 'Business and Finance'
       }, {
         //Computer and Mathetmatical Occupations
-        x: 78936, y: 68900, r: 6
+        x: 78936,
+        y: 68900,
+        r: 6,
+        label: 'Mathematics'
       }, {
-        //Computer Systems Analaysts
-        x: 76700, y: 69056, r: 6
+        //Computer Systems Analysts
+        x: 76700,
+        y: 69056,
+        r: 6,
+        label: 'Computer Systems Analyst'
       }, {
         //Software developers, applications and systems software
-        x: 96876, y: 80756, r: 6
+        x: 96876,
+        y: 80756,
+        r: 6,
+        label: 'Software Developer'
       }, {
         //Computer support specialists
-        x: 56056, y: 52728, r: 6
+        x: 56056,
+        y: 52728,
+        r: 6,
+        label: 'Computer Support'
       }, {
         //Computer occupations, all other
-        x: 64688, y: 54860, r: 6
+        x: 64688,
+        y: 54860,
+        r: 6,
+        label: 'Other Computer Occupations'
       }, {
         //Operations research analysts
-        x: 71864, y: 67600, r: 6
+        x: 71864,
+        y: 67600,
+        r: 6,
+        label: 'Operations Research Analyst'
       }, {
         //Architecture and engineering occupations
-        x: 79508, y: 62764, r: 6
+        x: 79508,
+        y: 62764,
+        r: 6,
+        label: 'Architecture and Engineering'
       }, {
         //Life, physical, and social science occupations
-        x: 68588, y: 58188, r: 6
+        x: 68588,
+        y: 58188,
+        r: 6,
+        label: 'Sciences'
       }, {
         //Community and social service occupations
-        x: 52208, y: 45760, r: 6
+        x: 52208,
+        y: 45760,
+        r: 6,
+        label: 'Community and Social Service'
       }, {
         //Legal occupations
-        x: 99008, y: 59904, r: 6
+        x: 99008,
+        y: 59904,
+        r: 6,
+        label: 'Legal'
       }, {
         //Education, training, and library occupations
-        x: 60008, y: 48516, r: 6
+        x: 60008,
+        y: 48516,
+        r: 6,
+        label: 'Education'
       }, {
         //Arts, design, entertainment, sports, and media occupations
-        x: 59228, y: 49452, r: 6
+        x: 59228,
+        y: 49452,
+        r: 6,
+        label: 'Arts, Media, and Sports'
       }, {
         //Healthcare practitioners and technical occupations
-        x: 69264, y: 53872, r: 6
+        x: 69264,
+        y: 53872,
+        r: 6,
+        label: 'Healthcare Practitioner'
       }, {
         //Healthcare support occupations
-        x: 31304, y: 26988, r: 6
+        x: 31304,
+        y: 26988,
+        r: 6,
+        label: 'Healthcare Support'
       }, {
         //Protective service occupations
-        x: 44408, y: 35776, r: 6
+        x: 44408,
+        y: 35776,
+        r: 6,
+        label: 'Protective Services'
       }, {
         //Food preparation and serving related occupations
-        x: 25532, y: 22568, r: 6
+        x: 25532,
+        y: 22568,
+        r: 6,
+        label: 'Food and Food Service'
       }, {
         //Building and grounds cleaning and maintenance occupations
-        x: 28496, y: 23608, r: 6
+        x: 28496,
+        y: 23608,
+        r: 6,
+        label: 'Building and Grounds Maintenance'
       }, {
         //Personal care and service occupations
-        x: 31876, y: 25064, r: 6
+        x: 31876,
+        y: 25064,
+        r: 6,
+        label: 'Personal Care and Service'
       }, {
         //Sales and related occupations
-        x: 47008, y: 30680, r: 6
+        x: 47008,
+        y: 30680,
+        r: 6,
+        label: 'Sales'
       }, {
         //Office and administrative support occupations
-        x: 36816, y: 34788, r: 6
+        x: 36816,
+        y: 34788,
+        r: 6,
+        label: 'Office and Administration'
       }, {
         //Natural resources, construction, and maintenance occupations
-        x: 41288, y: 30160, r: 6
+        x: 41288,
+        y: 30160,
+        r: 6,
+        label: 'Natural Resources and Construction'
       }, {
         //Production, transportation, and material moving occupations
-        x: 36608, y: 27612, r: 6
+        x: 36608,
+        y: 27612,
+        r: 6,
+        label: 'Transportation and Production'
       }],
         borderColor: "transparent",
-        backgroundColor: "rgb(255,90,90)",
+        backgroundColor: '#91A8d0',
         fill: false,
       },
       {
@@ -98,113 +186,103 @@ function renderRatioChart() {
           y: 100000
         }],
         borderColor: "transparent",
+        pointHoverRadius: 0,
       }]
 
-      var customTooltips = function(tooltip) {
-      			// Tooltip Element
-      			var tooltipEl = document.getElementById('chartjs-tooltip');
-      			if (!tooltipEl) {
-      				tooltipEl = document.createElement('div');
-      				tooltipEl.id = 'chartjs-tooltip';
-      				tooltipEl.innerHTML = "<table></table>"
-      				this._chart.canvas.parentNode.appendChild(tooltipEl);
-      			}
-      			// Hide if no tooltip
-      			if (tooltip.opacity === 0) {
-      				tooltipEl.style.opacity = 0;
-      				return;
-      			}
-      			// Set caret Position
-      			tooltipEl.classList.remove('above', 'below', 'no-transform');
-      			if (tooltip.yAlign) {
-      				tooltipEl.classList.add(tooltip.yAlign);
-      			} else {
-      				tooltipEl.classList.add('no-transform');
-      			}
-      			function getBody(bodyItem) {
-      				return bodyItem.lines;
-      			}
-      			// Set Text
-      			if (tooltip.body) {
-      				var titleLines = tooltip.title || [];
-      				var bodyLines = tooltip.body.map(getBody);
-      				var innerHtml = '<thead>';
-      				titleLines.forEach(function(title) {
-      					innerHtml += '<tr><th>' + title + '</th></tr>';
-      				});
-      				innerHtml += '</thead><tbody>';
-      				bodyLines.forEach(function(body, i) {
-      					var colors = tooltip.labelColors[i];
-      					var style = 'background:' + colors.backgroundColor;
-      					style += '; border-color:' + colors.borderColor;
-      					style += '; border-width: 2px';
-      					var span = '<span class="chartjs-tooltip-key" style="' + style + '"></span>';
-      					innerHtml += '<tr><td>' + span + body + '</td></tr>';
-      				});
-      				innerHtml += '</tbody>';
-      				var tableRoot = tooltipEl.querySelector('table');
-      				tableRoot.innerHTML = innerHtml;
-      			}
-      			var positionY = this._chart.canvas.offsetTop;
-      			var positionX = this._chart.canvas.offsetLeft;
-      			// Display, position, and set styles for font
-      			tooltipEl.style.opacity = 1;
-      			tooltipEl.style.left = positionX + tooltip.caretX + 'px';
-      			tooltipEl.style.top = positionY + tooltip.caretY + 'px';
-      			tooltipEl.style.fontFamily = tooltip._fontFamily;
-      			tooltipEl.style.fontSize = tooltip.fontSize;
-      			tooltipEl.style.fontStyle = tooltip._fontStyle;
-      			tooltipEl.style.padding = tooltip.yPadding + 'px ' + tooltip.xPadding + 'px';
-      		};
-
-  var payRatioChart = new Chart(payRatio, {
-      type: 'bubble',
-      data: {
-          datasets: data
-      },
-      options: {
-        title: {
-          display: true,
-          text: 'Gender Pay Gap'
-        },
-        tooltips: {
-          enabled: false,
-          mode: 'index',
-          position: 'nearest',
-          custom: customTooltips
-        },
-        showLines: true,
-        responsive: true,
-        maintainAspectRatio:false,
-        scales: {
-         xAxes: [{
-                 type: 'linear',
-                 position: 'bottom',
-                 ticks: {
-                     callback: function(label, index, labels) {
-                         return '$'+ label;
+      var payRatioChart = new Chart(payRatio, {
+          type: 'bubble',
+          data: {
+              datasets: data
+          },
+          options: {
+            title: {
+              display: true,
+              text: 'Gender Pay Gap'
+            },
+            tooltips: {
+              enabled: false,
+              mode: 'index',
+              position: 'nearest',
+              custom: function(tooltip) {
+                      // tooltip will be false if tooltip is not visible or should be hidden
+                      var tooltipEl = document.getElementById('chartjs-tooltip');
+                			if (!tooltipEl) {
+                				tooltipEl = document.createElement('div');
+                				tooltipEl.id = 'chartjs-tooltip';
+                				tooltipEl.innerHTML = "<table></table>"
+                				this._chart.canvas.parentNode.appendChild(tooltipEl);
+                			}
+                			// Hide if no tooltip
+                			if (tooltip.opacity === 0) {
+                				tooltipEl.style.opacity = 0;
+                				return;
+                			}
+                			// Set caret Position
+                			tooltipEl.classList.remove('above', 'below', 'no-transform');
+                			if (tooltip.yAlign) {
+                				tooltipEl.classList.add(tooltip.yAlign);
+                			} else {
+                				tooltipEl.classList.add('no-transform');
+                			}
+                			function getBody(bodyItem) {
+                				return bodyItem.lines;
+                			}
+                			// Set Text
+                			if (tooltip.body) {
+                				var titleLines = tooltip.title || [];
+                        var male = tooltip.body[0].lines[0].slice(19,24)
+                        var female = tooltip.body[0].lines[0].slice(26,31)
+                        var ratio = (female/male).toFixed(2)
+                				var bodyLines = tooltip.body.map(getBody);
+                        var occupation = 'Test'
+                        for (i = 0; i < 24; i++) {
+                          if (payRatioChart.data["datasets"][0]["data"][i]["x"] == male) {
+                            occupation = payRatioChart.data["datasets"][0]["data"][i]["label"];
+                          }
+                        }
+                        tooltipEl.innerHTML = '<tr><td><span class="chartjs-tooltip-key"></span>'+ occupation +'</td></tr><br><tr><td><span class="chartjs-tooltip-key"></span>Male: $'+ male +'</td></tr><br><tr><td><span class="chartjs-tooltip-key"></span>Female: $'+ female +'</td></tr><br><tr><td><span class="chartjs-tooltip-key"></span>Ratio: '+ ratio +'</td></tr>';
+                			}
+                			var positionY = this._chart.canvas.offsetTop;
+                			var positionX = this._chart.canvas.offsetLeft;
+                			// Display, position, and set styles for font
+                			tooltipEl.style.opacity = 1;
+                			tooltipEl.style.left = positionX + tooltip.caretX + 'px';
+                			tooltipEl.style.top = positionY + tooltip.caretY + 'px';
+                			tooltipEl.style.padding = tooltip.yPadding + 'px ' + tooltip.xPadding + 'px';
+                		}
+            },
+            showLines: true,
+            responsive: true,
+            maintainAspectRatio:false,
+            scales: {
+             xAxes: [{
+                     type: 'linear',
+                     position: 'bottom',
+                     ticks: {
+                         callback: function(label, index, labels) {
+                             return '$'+ label;
+                         }
+                     },
+                     scaleLabel: {
+                       display: true,
+                       labelString: 'Mens Median Annual Earnings'
                      }
-                 },
-                 scaleLabel: {
-                   display: true,
-                   labelString: 'Mens Median Annual Earnings'
-                 }
-               }],
-         yAxes: [{
-                 scaleLabel: {
-                   display: true,
-                   labelString: 'Womens Median Annual Earnings'
-                 },
-                 ticks: {
-                     callback: function(label, index, labels) {
-                         return '$' + label;
+                   }],
+             yAxes: [{
+                     scaleLabel: {
+                       display: true,
+                       labelString: 'Womens Median Annual Earnings'
+                     },
+                     ticks: {
+                         callback: function(label, index, labels) {
+                             return '$' + label;
+                         }
                      }
-                 }
-         }]
-       }
-      }
-  });
-};
+             }]
+           }
+          }
+      });
+    }
 
 function renderHoursChart() {
   $('#payRatio').remove();
@@ -216,11 +294,11 @@ function renderHoursChart() {
   var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: ["Management occupations", "Business and financial operations occupations", "Computer and mathematical occupations", "Computer systems analysts", "Computer programmers", "Software developers, applications and systems software", "Computer support specialists", "Computer occupations, all other", "Operations research analysts", "Architecture and engineering occupations", "Life, physical, and social science occupations", "Community and social service occupations", "Legal occupations", "Education, training, and library occupations", "Arts, design, entertainment, sports, and media occupations", "Healthcare practitioners and technical occupations", "Healthcare support occupations", "Protective service occupations", "Food preparation and serving related occupations", "Building and grounds cleaning and maintenance occupations", "Personal care and service occupations", "Sales and related occupations", "Office and administrative support occupations", "Natural resources, construction, and maintenance occupations", "Production, transportation, and material moving occupations"],
+      labels: ["Management", "Business and Finance", "Mathematics", "Computer Systems Analyst", "Software Developer", "Computer Support", "Other Computer Occupations", "Operations Research Analyst", "Architecture and Engineering", "Sciences", "Community and Social Service", "Legal", "Education", "Arts, Media, and Sports", "Healthcare Practitioner", "Healthcare Support", "Protective Services", "Food and Food Service", "Building and Grounds Maintenance", "Personal Care and Service", "Sales", "Office and Administration", "Natural Resources and Construction", "Production and Transportation"],
       datasets: [{
         label: 'Extra hours',
         backgroundColor: "rgba(52,187,199,0.8)",
-        data: [1.340592334, 1.348722986, 1.145660377, 1.110692771, 1.117378049, 1.199613651, 1.063116371, 1.179146919, 1.063076923, 1.266777133, 1.17873101, 1.140909091, 1.652777778, 1.236870311, 1.197686646, 1.285714286, 1.159922929, 1.24127907, 1.131336406, 1.207048458, 1.271784232, 1.53220339, 1.058295964, 1.368965517, 1.325800377]
+        data: [1.34, 1.35, 1.15, 1.11, 1.20, 1.06, 1.18, 1.06, 1.27, 1.18, 1.14, 1.65, 1.24, 1.20, 1.29, 1.16, 1.24, 1.13, 1.21, 1.27, 1.53, 1.06, 1.37, 1.33]
       },]
     },
     options: {
